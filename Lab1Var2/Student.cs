f8126bb7d3a10d3e08f1d3adff8bfb84a06718a3
Alex Bugay <bosco.bugai@gmail.com>
2023-02-01 22:47:41 +0200

    intro Class

---
+++ Lab1Var2/Student.cs
@@ -0,0 +1,62 @@
+﻿/*Розробити консольний застосунок для роботи з класом
+Student з полями: name, age, course, rating.
+Всі поля класу зробити закритими, а доступ до них реалізувати
+через шаблони властивостей. Реалізувати конструктор з
+параметрами.
+Створити методи:
+EditStudent для редагування даних. Метод має 3 параметри
+для зміни значень полів name, age, rating.
+StudentRating(double rating), який виводить рейтинг студента.
+GetRole(int course) – повертає рядок: бакалавр чи магістр
+залежно від курсу.*/
+
+namespace Task1;
+class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Role { get; set; }
+    public int Course { get; set; }
+    public double Rating { get; set; }
+    public Student(string name, int age, int course, double rating)
+    {
+        Name = name;
+        Age = age;
+        Course = course;
+        Rating = rating;
+    }
+
+    public void EditStudent(string name, int age, double rating)
+    {
+        Name = name;
+        Age = age;
+        Rating = rating;
+    }
+    public string GetRole(int course)
+    {
+        if (course <= 4)
+            Role = "бакалавр";
+        else
+            Role = "магістр";
+        return Role;
+    }
+    public void Student_Rating()
+    {
+        if (Rating >= 82)
+            Console.WriteLine("Привіт відмінникам");
+        else
+            if (Rating <= 60)
+            Console.WriteLine("Перездача! Треба краще вчитися!");
+        else
+            Console.WriteLine("Можна вчитися ще краще!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student stud = new Student("Tom", 23, 4, 85);
+        Console.WriteLine($"Студент {stud.Name}, {stud.Course} курсу");
+    }
+}
---
+++ Task2/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Розробити клас Тварина з 4 полями і двома методами.
+Поля: ім’я, вага, вік, вид.
+Доступ до полів реалізувати через властивості get i set.
+Реалізувати конструктор без параметрів, а ініціалізацію полів
+виконати через властивості у коді програми.
+Методи:
+Перший метод виводить на консоль вагу тварини.
+Другий метод виводить на консоль запит “Що це за тварина?”
+Сформувати рядок “Це тварина” + "вид" + "ім’я" і вивести
+його на консоль.*/
+
+namespace Task2;
+class Animal
+{
+    public string Name { get; set; }
+    public double Weight { get; set; }
+    public int Age { get; set; }
+    public string Type { get; set; }
+
+    public Animal() { }
+
+    public void AnimalWeight() { Console.WriteLine(Weight); }
+    public void WhoIsIt()
+    {
+        Console.WriteLine("Цо це за тварина?");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animal = new Animal() { Name = "Мурка" };
+        animal.Weight = 60;
+        animal.AnimalWeight();
+        animal.Type = Console.ReadLine();
+        Console.WriteLine($"Це тварина  {animal.Type} {animal.Name}");
+    }
+}
